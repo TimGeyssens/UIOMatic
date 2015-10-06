01f9ed3144633db3019d44803fd1d790e8892241
TimGeyssens <tg@umbraco.com>
2015-10-06 14:09:56 +0200

    Adds readme

---
+++ Readme.md
@@ -0,0 +1,61 @@
+# UI-O-Matic #
+
+----------
+
+**Auto generate an integrated crud UI in Umbraco for a db table based on a petapoco poco**
+
+Implement an interface and decorate your class and properties with some additional attributes.
+
+## Example ##
+
+
+    [UIOMaticAttribute("People","icon-users","icon-user")]
+    [TableName("People")]
+    public class Person: IUIOMaticModel
+    {
+        public Person() { }
+
+        [UIOMaticIgnoreField]
+        [PrimaryKeyColumn(AutoIncrement = true)]
+        public int Id { get; set; }
+
+        [UIOMaticField("First name","Enter the persons first name")]
+        public string FirstName { get; set; }
+
+        [UIOMaticField("Last name", "Enter the persons last name")]
+        public string LastName { get; set; }
+
+        [UIOMaticField("Picture", "Select a picture", View = "file")]
+        public string Picture { get; set; }
+
+        [Ignore]
+        public string UmbracoTreeNodeName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+
+
+        public IEnumerable<Exception> Validate()
+        {
+            var exs = new List<Exception>();
+
+            if(string.IsNullOrEmpty(FirstName))
+                exs.Add(new Exception("Please provide a value for first name"));
+
+            if (string.IsNullOrEmpty(LastName))
+                exs.Add(new Exception("Please provide a value for last name"));
+
+
+            return exs;
+        }
+    }`
+
+Will generate the following UI
+
+![](example.png)
+
+### Test site ###
+Backoffice credentials: 
+- tim@nibble.be / password
+
+
--- src/UIOMatic/Models/Dog.cs
+++ src/UIOMatic/Models/Dog.cs
@@ -1,50 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using UIOMatic.Atributes;
-using UIOMatic.Interfaces;
-using Umbraco.Core.Persistence;
-using Umbraco.Core.Persistence.DatabaseAnnotations;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using UIOMatic.Atributes;
+//using UIOMatic.Interfaces;
+//using Umbraco.Core.Persistence;
+//using Umbraco.Core.Persistence.DatabaseAnnotations;
 
-namespace UIOMatic.Models
-{
-    [UIOMatic("Dogs", "icon-users", "icon-user")]
-    [TableName("Dogs")]
-    public class Dog : IUIOMaticModel
-    {
-        public Dog() { }
+//namespace UIOMatic.Models
+//{
+//    [UIOMatic("Dogs", "icon-users", "icon-user")]
+//    [TableName("Dogs")]
+//    public class Dog : IUIOMaticModel
+//    {
+//        public Dog() { }
 
-        [UIOMaticIgnoreField]
-        [PrimaryKeyColumn(AutoIncrement = true)]
-        public int Id { get; set; }
-
-
-        public string Name { get; set; }
+//        [UIOMaticIgnoreField]
+//        [PrimaryKeyColumn(AutoIncrement = true)]
+//        public int Id { get; set; }
 
 
-        public string OwnerName { get; set; }
+//        public string Name { get; set; }
 
 
-        [Ignore]
-        public string UmbracoTreeNodeName
-        {
-            get { return Name; }
-        }
+//        public string OwnerName { get; set; }
 
 
-        public IEnumerable<Exception> Validate()
-        {
-            var exs = new List<Exception>();
-
-            if (string.IsNullOrEmpty(Name))
-                exs.Add(new Exception("Please provide a value for name"));
-
-            if (string.IsNullOrEmpty(OwnerName))
-                exs.Add(new Exception("Please provide a value for owner name"));
+//        [Ignore]
+//        public string UmbracoTreeNodeName
+//        {
+//            get { return Name; }
+//        }
 
 
-            return exs;
-        }
-    }
-}
+//        public IEnumerable<Exception> Validate()
+//        {
+//            var exs = new List<Exception>();
+
+//            if (string.IsNullOrEmpty(Name))
+//                exs.Add(new Exception("Please provide a value for name"));
+
+//            if (string.IsNullOrEmpty(OwnerName))
+//                exs.Add(new Exception("Please provide a value for owner name"));
+
+
+//            return exs;
+//        }
+//    }
+//}
