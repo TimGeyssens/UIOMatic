e247cb44db6600c55762981ed7407b90591be3be
TimGeyssens <tg@umbraco.com>
2015-10-06 16:48:35 +0200

    build script should be good now

--- src/UIOMatic/Properties/VersionInfo.cs
+++ src/UIOMatic/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.942.45454")]
+[assembly: AssemblyFileVersion("0.1.942.45879")]
 [assembly: AssemblyInformationalVersion("0.1.0")]
 
 
