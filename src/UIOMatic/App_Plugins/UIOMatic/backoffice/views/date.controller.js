6e858c7ca69445f0d1158ab2b18ba259e1395851
TimGeyssens <tg@umbraco.com>
2015-10-08 12:56:26 +0200

    work on datepicker field

---
+++ src/UIOMatic/App_Plugins/UIOMatic/backoffice/views/date.controller.js
@@ -0,0 +1,22 @@
+﻿angular.module("umbraco").controller("UIOMatic.Views.Date",
+	function ($scope) {
+
+	    angular.element(document).ready(function () {
+	        
+            //doesn't work without the timeout :(
+	        setTimeout(function() {
+	          
+	            var picker = new Pikaday(
+	            {
+	                field: document.getElementById("UIOMaticProperty" + $scope.property.Name),
+	                format: 'DD/MMM/YYYY',
+	                firstDay: 1,
+	                
+	            });
+
+	        }, 100);
+
+	        
+	    });
+
+	});
