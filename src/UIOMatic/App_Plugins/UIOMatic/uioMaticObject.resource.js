0c62e99ffb7ff1b08ec6ba798f629e8cb67da1ea
David Brendel <mantus667@gmail.com>
2015-10-08 19:27:37 +0200

    Added ServerVariableParser
    Added ServerVariableParser for better url handling

--- src/UIOMatic/App_Plugins/UIOMatic/uioMaticObject.resource.js
+++ src/UIOMatic/App_Plugins/UIOMatic/uioMaticObject.resource.js
@@ -6,37 +6,37 @@
 	                sortColumn = "";
 	            if (sortOrder == undefined)
 	                sortOrder = "";
-	            return $http.get("backoffice/UIOMatic/PetaPocoObject/GetAll?typeName=" + type + "&sortColumn=" + sortColumn + "&sortOrder="+sortOrder);
+	            return $http.get(Umbraco.Sys.ServerVariables.uioMatic.ppcBaseUrl + "GetAll?typeName=" + type + "&sortColumn=" + sortColumn + "&sortOrder="+sortOrder);
 	        },
 	        getAllProperties: function (type) {
-	            return $http.get("backoffice/UIOMatic/PetaPocoObject/GetAllProperties?typeName=" + type);
+	            return $http.get(Umbraco.Sys.ServerVariables.uioMatic.ppcBaseUrl + "GetAllProperties?typeName=" + type);
 	        },
 	        getById: function (type, id) {
-	            return $http.get("backoffice/UIOMatic/PetaPocoObject/GetById?typeName="+type+"&id=" + id);
+	            return $http.get(Umbraco.Sys.ServerVariables.uioMatic.ppcBaseUrl + "GetById?typeName=" + type + "&id=" + id);
 	        },
 	        getPrimaryKeyColumnName: function(type) {
-	            return $http.get("backoffice/UIOMatic/PetaPocoObject/GetPrimaryKeyColumnName?typeName=" + type);
+	            return $http.get(Umbraco.Sys.ServerVariables.uioMatic.ppcBaseUrl + "GetPrimaryKeyColumnName?typeName=" + type);
 	        },
 	        create: function (type, object) {
 	            var item = {};
 	            item.typeOfObject = type;
 	            item.objectToCreate = object;
-	            return $http.post("backoffice/UIOMatic/PetaPocoObject/PostCreate", angular.toJson(item));
+	            return $http.post(Umbraco.Sys.ServerVariables.uioMatic.ppcBaseUrl + "PostCreate", angular.toJson(item));
 	        },
 	        update: function (type, object) {
 	            var item = {};
 	            item.typeOfObject = type;
 	            item.objectToUpdate = object[0];
-	            return $http.post("backoffice/UIOMatic/PetaPocoObject/PostUpdate", angular.toJson(item));
+	            return $http.post(Umbraco.Sys.ServerVariables.uioMatic.ppcBaseUrl + "PostUpdate", angular.toJson(item));
 	        },
 	        deleteById: function (type, id) {
-                return $http.delete("backoffice/UIOMatic/PetaPocoObject/DeleteById?typeOfObject=" + type + "&id=" + id);
+	            return $http.delete(Umbraco.Sys.ServerVariables.uioMatic.ppcBaseUrl + "DeleteById?typeOfObject=" + type + "&id=" + id);
 	        },
 	        validate: function (type, object) {
 	            var item = {};
 	            item.typeOfObject = type;
 	            item.objectToValidate = object;
-	            return $http.post("backoffice/UIOMatic/PetaPocoObject/Validate", angular.toJson(item));
+	            return $http.post(Umbraco.Sys.ServerVariables.uioMatic.ppcBaseUrl + "Validate", angular.toJson(item));
 	        }
 	    };
 	});
