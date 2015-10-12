dae7e420bc8e36ba09990cca5c3205cb42491472
TimGeyssens <tg@umbraco.com>
2015-10-12 11:03:00 +0200

    ok now the datetime picker is working

--- src/UIOMatic/App_Plugins/UIOMatic/backoffice/uioMaticTree/edit.controller.js
+++ src/UIOMatic/App_Plugins/UIOMatic/backoffice/uioMaticTree/edit.controller.js
@@ -120,8 +120,8 @@ angular.module("umbraco").controller("uioMatic.ObjectEditController",
 	                        for (var prop in $scope.properties) {
 	                            if ($scope.properties[prop].Key == theKey) {
 	                                if ($scope.properties[prop].Type == "System.DateTime") {
-	                                    var date = new Date($scope.object[theKey]);
-	                                    $scope.properties[prop].Value = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDay() + " " + date.getHours() + ":" + date.getMinutes() + ":" +date.getSeconds();
+	                                    var date = moment($scope.object[theKey]).format("YYYY-MM-DD HH:mm:ss");
+	                                    $scope.properties[prop].Value = date;
 	                                } else {
 	                                    $scope.properties[prop].Value = $scope.object[theKey];
 	                                }
