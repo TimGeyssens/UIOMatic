e0ea729c39122672b33d408bce7b885243ade5a6
TimGeyssens <tg@umbraco.com>
2015-10-23 16:04:46 +0200

    Adds radiobuttonlist property view and controller

---
+++ src/UIOMatic/App_Plugins/UIOMatic/backoffice/views/radiobuttonlist.controller.js
@@ -0,0 +1,25 @@
+﻿angular.module("umbraco").controller("UIOMatic.Views.Radiobuttonlist",
+    function ($scope, uioMaticObjectResource) {
+        //example config
+        //"{'typeName':'Example.Models.Person, Example', 'valueColumn': 'Id', 'textColumn'='FirstName'}"
+
+       
+
+        function init() {
+            uioMaticObjectResource.getAll($scope.property.Config.typeName, $scope.property.Config.textColumn, "asc").then(function (response) {
+                $scope.objects = response.data;
+
+            });
+        }
+
+        init();
+
+        $scope.$on('ValuesLoaded', function (event, data) {
+            init();
+        });
+
+        $scope.setValue = function (val) {
+
+            $scope.property.Value = val;
+        }
+    });
