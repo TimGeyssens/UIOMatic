de05367abb3abffb79efa97be207a56f421faa5a
serenabenny <netpayment@gmail.com>
2015-12-19 12:00:52 +0800

    commit missing file
    commit missing file

--- src/UIOMatic/App_Plugins/UIOMatic/backoffice/uioMaticTree/list.controller.js
+++ src/UIOMatic/App_Plugins/UIOMatic/backoffice/uioMaticTree/list.controller.js
@@ -1,5 +1,5 @@
 ﻿angular.module("umbraco").controller("uioMatic.ObjectListController",
-    function($scope, $routeParams, uioMaticObjectResource) {
+    function ($scope, $routeParams, uioMaticObjectResource) {
 
         $scope.typeName = $routeParams.id;
         $scope.selectedIds = [];
@@ -8,15 +8,29 @@
         $scope.currentPage = 1;
         $scope.itemsPerPage = 10;
         $scope.totalPages = 1;
-        
+
         $scope.reverse = false;
+
+        $scope.isFilterForm = false;
 
         $scope.searchTerm = "";
 
+        $scope.displayname = "";
+
+        $scope.Operators =
+        [
+            { id: 1, name: "=" },
+            { id: 2, name: "≠" },
+            { id: 3, name: ">" },
+            { id: 4, name: "<" },
+            { id: 5, name: "≥" },
+            { id: 6, name: "≤" }
+        ];
+
         function fetchData() {
-            
-            uioMaticObjectResource.getPaged($scope.typeName, $scope.itemsPerPage, $scope.currentPage, $scope.predicate, $scope.reverse ? "desc" : "asc", $scope.searchTerm).then(function (resp) {
-               
+
+            uioMaticObjectResource.getQuery($routeParams.id, $scope.filterproperties, $scope.itemsPerPage, $scope.currentPage, $scope.predicate, $scope.reverse ? "desc" : "asc", $scope.searchTerm).then(function (resp) {
+
                 $scope.rows = resp.data.Items;
                 $scope.totalPages = resp.data.TotalPages;
 
@@ -33,8 +47,14 @@
             $scope.predicate = response.data.PrimaryKeyColumnName.replace(' ', '_');
             $scope.ignoreColumnsFromListView = response.data.IgnoreColumnsFromListView;
             $scope.nameField = response.data.NameField.replace(' ', '_');
-            $scope.readOnly = response.data.ReadOnly
+            $scope.readOnly = response.data.ReadOnly;
             fetchData();
+            uioMaticObjectResource.getFilterProperties($scope.typeName).then(function (response) {
+                $scope.filterproperties = response.data;
+                if ($scope.filterproperties.length > 0) {
+                    $scope.isFilterForm = true;
+                }
+            });
 
         });
 
@@ -52,11 +72,28 @@
 
         }
 
+        $scope.filter = function (object) {
+            fetchData();
+            //if (confirm("Are you sure you want to query data?")) {
+            //    uioMaticObjectResource.getQuery($routeParams.id, $scope.filterproperties, $scope.itemsPerPage, $scope.currentPage, $scope.predicate, $scope.reverse ? "desc" : "asc",$scope.searchTerm).then(function (resp) {
+            //        $scope.rows = resp.data.Items;
+            //        $scope.totalPages = resp.data.TotalPages;
+
+            //        if ($scope.rows.length > 0) {
+            //            $scope.cols = Object.keys($scope.rows[0]).filter(function (c) {
+            //                return $scope.ignoreColumnsFromListView.indexOf(c) == -1;
+            //            });
+            //        }
+
+            //    });
+            //}
+        }
+
         $scope.delete = function (object) {
             if (confirm("Are you sure you want to delete " + $scope.selectedIds.length + " object" + ($scope.selectedIds.length > 1 ? "s" : "") + "?")) {
                 $scope.actionInProgress = true;
                 var keyPropName = $scope.primaryKeyColumnName;
-                uioMaticObjectResource.deleteByIds($routeParams.id, $scope.selectedIds).then(function() {
+                uioMaticObjectResource.deleteByIds($routeParams.id, $scope.selectedIds).then(function () {
                     $scope.rows = _.reject($scope.rows, function (el) { return $scope.selectedIds.indexOf(el[keyPropName]) > -1; });
                     $scope.selectedIds = [];
                     $scope.actionInProgress = false;
@@ -106,18 +143,18 @@
             fetchData();
         };
 
-        $scope.search = function(searchFilter) {
+        $scope.search = function (searchFilter) {
             $scope.searchTerm = searchFilter;
             $scope.currentPage = 1;
             fetchData();
         };
 
         $scope.isColumnLinkable = function (column, index) {
-           
+
             if ($scope.nameField.length > 0) {
                 return column == $scope.nameField;
             } else {
-               
+
                 return index == 0
                 || (index == 1 && $scope.cols[0] == $scope.primaryKeyColumnName)
             }
