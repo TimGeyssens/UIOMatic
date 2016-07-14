249c52361e296019d9671501e327a79febf6ef0a
TimGeyssens <tg@umbraco.com>
2016-07-14 13:45:25 +0200

    Adds un camel case option to the list view editor

--- src/UIOMatic/App_Plugins/UIOMatic/backoffice/uioMaticTree/list.controller.js
+++ src/UIOMatic/App_Plugins/UIOMatic/backoffice/uioMaticTree/list.controller.js
@@ -127,4 +127,15 @@
                 || (index == 1 && $scope.cols[0] == $scope.primaryKeyColumnName)
             }
         }
+
+        $scope.unCamelCase = function(str)
+        {
+            return str
+                // insert a space between lower & upper
+                .replace(/([a-z])([A-Z])/g, '$1 $2')
+                // space before last upper in a sequence followed by lower
+                .replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3')
+                // uppercase the first character
+                .replace(/^./, function(str){ return str.toUpperCase(); })
+        }
     });
