2b56e615d69d5ccc33bd2c4b37bdf0bec12ace91
mattbrailsford <me@mattbrailsford.com>
2016-10-06 09:18:00 +0100

    Fixing casing issues

--- src/UIOMatic/Sections/Section.cs
+++
@@ -1,9 +0,0 @@
-﻿using umbraco.businesslogic;
-using umbraco.interfaces;
-
-namespace UIOMatic.Sections
-{
-    [Application("uiomatic", "UI-O-Matic", "icon-wand", 15)]
-    public class Section : IApplication { }
-
-}
