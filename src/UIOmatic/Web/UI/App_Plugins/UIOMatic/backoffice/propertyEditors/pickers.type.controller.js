54125ca9f045ac3c0b72fb37a0530b9d0ddd5cae
mattbrailsford <me@mattbrailsford.com>
2016-10-07 15:53:06 +0100

    Removed console.log

--- src/UIOmatic/Web/UI/App_Plugins/UIOMatic/backoffice/propertyEditors/pickers.type.controller.js
+++ src/UIOmatic/Web/UI/App_Plugins/UIOMatic/backoffice/propertyEditors/pickers.type.controller.js
@@ -1,6 +1,5 @@
 ﻿angular.module("umbraco").controller("UIOMatic.PropertyEditors.Pickers.Type",
 	function ($scope, $http) {
-	    console.log($scope);
 	    $http.get(Umbraco.Sys.ServerVariables.uioMatic.pecBaseUrl + "GetAllTypes").then(function (response) {
 	        $scope.types = response.data;
 	    });
