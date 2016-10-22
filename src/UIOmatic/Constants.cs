13dbaed16d90b40fcc35e0a684cc30b4fc1f9a76
mattbrailsford <me@mattbrailsford.com>
2016-10-22 14:33:59 +0100

    Added google maps view

--- src/UIOmatic/Constants.cs
+++ src/UIOmatic/Constants.cs
@@ -4,6 +4,7 @@ namespace UIOMatic
 {
     public static class Constants
     {
+        // TODO: Add paths to all built in views
         public static readonly IReadOnlyDictionary<string, string> Views = new Dictionary<string, string>
         {
             { "textfield", "~/App_Plugins/UIOMatic/Backoffice/Views/checkbox.html"},
