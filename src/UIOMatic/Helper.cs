e30f118013149609e8f8f11c8ebc6c0d92e4da27
mattbrailsford <me@mattbrailsford.com>
2016-10-26 08:19:38 +0100

    Added check to EnsureUIOMaticTypes method to make sure all UIOMatic model types have a unique alias. Fixes #63

--- src/UIOMatic/Helper.cs
+++ src/UIOMatic/Helper.cs
@@ -33,6 +33,17 @@ namespace UIOMatic
         private static IEnumerable<Type> EnsureUIOMaticTypes()
         {
             var t = TypeFinder.FindClassesWithAttribute<UIOMaticFolderAttribute>(); // UIOMaticFolderAttribute is the base type for all UIOMatic entities
+
+            // Ensure unique aliases
+            var aliases = new List<string>();
+            foreach (var attr in t.Select(type => type.GetCustomAttribute<UIOMaticFolderAttribute>(true)))
+            {
+                if (aliases.Any(x => x == attr.Alias))
+                    throw new ApplicationException("Multiple UI-O-Matic model types found with alias '"+ attr.Alias +"'. Please ensure all types have a unique alias value.");   
+                
+                aliases.Add(attr.Alias);
+            }
+
             HttpRuntime.Cache.Insert("UIOMaticFolderTypes", t);
             return t;
         }
