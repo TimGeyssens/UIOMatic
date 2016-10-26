0be14b0fee4fb28f34ee83438fe3f18bad327b00
mattbrailsford <me@mattbrailsford.com>
2016-10-26 08:10:56 +0100

    Set the foundations for issue #61 formatting of the label value, but need to get clarification on exactly what formats to support.

---
+++ src/UIOMatic/Web/UI/App_Plugins/UIOMatic/backoffice/views/fieldeditors/label.controller.js
@@ -0,0 +1,19 @@
+﻿angular.module("umbraco").controller("UIOMatic.Views.Label",
+    function ($scope) {
+
+        function init() {
+            if ($scope.property.config && $scope.property.config.format) {
+                // TODO: Format value
+                $scope.labelValue = $scope.property.value;
+            } else {
+                $scope.labelValue = $scope.property.value;
+            }
+        }
+
+        init();
+
+        $scope.$on('valuesLoaded', function (event, data) {
+            init();
+        });
+        
+    });
