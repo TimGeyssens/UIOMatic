ce9733fa6075affe44702d6db87dd155ad9aa738
LAPTOP-JSAC0SHM\Tim Geyssens <tim@nibble.be>
2017-10-18 10:29:20 +0200

    Make sure UIOMatic can find all the types (ones that are created with reflection) + updates version number to 2.0.4

--- src/UIOMatic/Helper.cs
+++ src/UIOMatic/Helper.cs
@@ -32,20 +32,25 @@ namespace UIOMatic
 
         private static IEnumerable<Type> EnsureUIOMaticTypes()
         {
-            var t = TypeFinder.FindClassesWithAttribute<UIOMaticFolderAttribute>(); // UIOMaticFolderAttribute is the base type for all UIOMatic entities
+            var typesWithMyAttribute =
+                from a in AppDomain.CurrentDomain.GetAssemblies()
+                from t in a.GetTypes()
+                let attributes = t.GetCustomAttributes(typeof(UIOMaticFolderAttribute), true)
+                where attributes != null && attributes.Length > 0
+                select t; // UIOMaticFolderAttribute is the base type for all UIOMatic entities
 
             // Ensure unique aliases
             var aliases = new List<string>();
-            foreach (var attr in t.Select(type => type.GetCustomAttribute<UIOMaticFolderAttribute>(true)))
+            foreach (var attr in typesWithMyAttribute.Select(type => type.GetCustomAttribute<UIOMaticFolderAttribute>(true)))
             {
                 if (aliases.Any(x => x == attr.Alias))
-                    throw new ApplicationException("Multiple UI-O-Matic model types found with alias '"+ attr.Alias +"'. Please ensure all types have a unique alias value.");   
-                
+                    throw new ApplicationException("Multiple UI-O-Matic model types found with alias '" + attr.Alias + "'. Please ensure all types have a unique alias value.");
+
                 aliases.Add(attr.Alias);
             }
 
-            HttpRuntime.Cache.Insert("UIOMaticFolderTypes", t);
-            return t;
+            HttpRuntime.Cache.Insert("UIOMaticFolderTypes", typesWithMyAttribute);
+            return typesWithMyAttribute;
         }
 
         public static Type GetUIOMaticTypeByAlias(string typeAlias, bool includeFolders = false, bool throwNullError = false)
