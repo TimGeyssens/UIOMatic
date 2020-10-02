21de93494ff092b701ea9eb74024adedecd83ea1
Mario Lopez <mario@monkii.com>
2020-10-03 06:37:42 +1000

    moved the logic to the right controller

--- src/UIOMatic/Web/UI/App_Plugins/UIOMatic/backoffice/apps/uiomaticContent.controller.js
+++ src/UIOMatic/Web/UI/App_Plugins/UIOMatic/backoffice/apps/uiomaticContent.controller.js
@@ -1,5 +1,32 @@
 ﻿angular.module("umbraco").controller("uiomaticContentController",
-    function ($scope, $routeParams, $location, $timeout, editorState, uioMaticObjectResource, notificationsService, navigationService) {
+    function ($scope, editorState) {
+        
+        $scope.properties = editorState.current.type.editableProperties;
+        $scope.queryString = editorState.current.qs;             
 
-        var test= editorState;
+        var tabsArr = [];
+
+        angular.forEach($scope.properties, function (value, key) {
+            if (this.map(function (e) { return e.label; }).indexOf(value.tab) === -1) {
+                if (value.tab == "") {
+                    this.push({ id: 99, label: "General" });
+                } else {
+                    this.push({ id: value.tabOrder > 0 ? value.tabOrder : key, label: value.tab, active: $scope.queryString["tab"] === "tab" + value.tabOrder });
+                }
+            }
+
+        }, tabsArr);
+
+        if (tabsArr.length > 1) {
+            $scope.content = {
+                tabs: tabsArr.sort(function (a, b) {
+                    if (a.id < b.id)
+                        return -1;
+                    if (a.id > b.id)
+                        return 1;
+                    return 0;
+                })
+            };
+        }
+
     });
--- src/UIOMatic/Web/UI/App_Plugins/UIOMatic/backoffice/uiomatic/edit.controller.js
+++ src/UIOMatic/Web/UI/App_Plugins/UIOMatic/backoffice/uiomatic/edit.controller.js
@@ -46,9 +46,13 @@
             $scope.properties = response.editableProperties;
             $scope.type.nameFieldIndex = $scope.type.nameFieldKey.length > 0
                 ? _.indexOf(_.pluck($scope.properties, "key"), $scope.type.nameFieldKey)
-                : -1;
+                : -1;            
 
-            editorState.set = response;
+            editorState.set({                
+                qs,                
+                type: response
+            });
+
             var tabsArr = [];
 
             // Build items path
@@ -61,30 +65,7 @@
                 navigationService.syncTree({ tree: 'uiomatic', path: $scope.path, forceReload: false, activate: true });
             }
 
-            angular.forEach($scope.properties, function (value, key) {
-                if (this.map(function (e) { return e.label; }).indexOf(value.tab) === -1) {
-                    if (value.tab == "") {
-                        this.push({ id: 99, label: "General" });
-                    } else {
-                        this.push({ id: value.tabOrder > 0 ? value.tabOrder : key, label: value.tab, active: $scope.queryString["tab"] === "tab" + value.tabOrder });
-                    }
-                }
-
-            }, tabsArr);
-
-            if (tabsArr.length > 1) {
-                $scope.content = {
-                    tabs: tabsArr.sort(function (a, b) {
-                        if (a.id < b.id)
-                            return -1;
-                        if (a.id > b.id)
-                            return 1;
-                        return 0;
-                    })
-                };
-            }
-
-
+          
             if (!hasId) {
                 uioMaticObjectResource.getScaffold($scope.typeAlias).then(function (response) {
                     $scope.object = response;
@@ -153,7 +134,7 @@
                 }
             });
 
-            $scope.$broadcast("editors.apps.appChanged", { app: activeApp });
+            $scope.$broadcast("editors.apps.appChanged", { app: activeApp });            
         };
 
 
