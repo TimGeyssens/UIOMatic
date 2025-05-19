c9eef0f8251dd9a58a02c63dcbf009d51e003e81
Tim Geyssens <timgeyssens@Tims-MacBook-Air.local>
2025-05-19 19:06:45 +0200

    Fix TinyMCE Vue integration

--- src/UIOMatic.SPA/src/components/fields/RichTextField.vue
+++ src/UIOMatic.SPA/src/components/fields/RichTextField.vue
@@ -5,13 +5,13 @@
 </template>
 
 <script>
-import { Editor } from '@tinymce/tinymce-vue'
+import { Editor as TinyMCEEditor } from '@tinymce/tinymce-vue'
 import { rteConfig } from '../../config'
 
 export default {
   name: 'RichTextField',
   components: {
-    Editor
+    Editor: TinyMCEEditor
   },
   props: {
     modelValue: {
